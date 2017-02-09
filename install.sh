280e3a13a664ecca24fbb26810b5b6e100dd0313
Léo G <leograndcontact@gmail.com>
2017-02-09 09:41:00 +0000

    yarn setup

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 echo 'Installing packages...'
 ### packages install
 sudo pacman -Syu git vim vlc yaourt firefox zsh
-yaourt -S antigen-git oh-my-zsh-git visual-studio-code sublime-text-dev spotify deluge
+yaourt -S yarn antigen-git oh-my-zsh-git visual-studio-code sublime-text-dev spotify deluge
 
 echo 'Configurating vim&zsh'
 ### Config setup
@@ -22,6 +22,16 @@ cp zshrc $HOME/.zshrc
 cp vimrc $HOME/.vimrc
 rm vimrc
 rm zshrc
+
+echo 'Yarn global config'
+mkdir $HOME/.yarn-global
+yarn config set prefix $HOME/.yarn-global
+echo "export PATH="$PATH:yarn global bin"" >> $HOME/.zshrc
+echo "export PATH="$PATH:yarn global bin"" >> $HOME/.bashrc
+echo "Yarn ready!"
+
+echo "Reloading bash"
+source $HOME/.zshrc
 
 echo 'Finished!'
 exit 0
